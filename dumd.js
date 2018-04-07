d93ffb50f2855c31184f153ff641567b1d0cbb8b
Gregory Skiano <gregoryskiano@gmail.com>
2018-04-07 09:25:47 -0700

    smaller

--- dumd.js
+++ dumd.js
@@ -2,13 +2,14 @@
 // TODO support magic 'require' maybe?
 // TODO either handle cyclic deps or throw if they exist
 
+// n = needs
+// d = deps
+// b = body
+// f = factory
+
 (function define(
   modules,
   pending,
-  needs,
-  deps,
-  body,
-  factory,
   window,
   exports,
   define,
@@ -32,30 +33,30 @@
   instantiate = (id, /* declarations: */ thisBody, thisModule, exportsObject, args, stillPending) => {
     thisModule = modules[id]
 
-    if (isEmpty(thisModule[deps])) {
-      thisBody = thisModule[factory]()
+    if (isEmpty(thisModule.d)) {
+      thisBody = thisModule.f()
     } else {
       args = []
       exportsObject
-      each(thisModule[deps], (d) => {
+      each(thisModule.d, (d) => {
         if (d === exports) {
           exportsObject = {}
           args[push](exportsObject)
         } else {
-          args[push](modules[d][body])
+          args[push](modules[d].b)
         }
       })
-      thisBody = thisModule[factory][apply](null, args)
+      thisBody = thisModule.f[apply](null, args)
       if (exportsObject) thisBody = exportsObject
     }
-    thisModule[body] = thisBody
+    thisModule.b = thisBody
     window[id] = thisBody
 
     stillPending = []
     each(pending, (otherModuleId, /* declarations */ mod, undef) => {
       mod = modules[otherModuleId]
-      if (mod[needs][id]) {
-        mod[needs][id] = undef
+      if (mod.n[id]) {
+        mod.n[id] = undef
         mod.c -= 1
         if (mod.c === 0) {
           instantiate(otherModuleId)
@@ -71,19 +72,19 @@
     if (modDeps[apply]) modFactory = modDeps, modDeps = []
 
     thisModule = modules[moduleId] = { id: moduleId, c: 0 }
-    thisModule[factory] = modFactory
-    thisModule[deps] = modDeps
-    thisModule[needs] = {}
+    thisModule.f = modFactory
+    thisModule.d = modDeps
+    thisModule.n = {}
 
     // what, if anything does this module need?
     // each(deps, () => {})
     each(modDeps, (need) => {
       if (
         !modules[need] || // the module isnt here yet
-        !isEmpty(modules[need][needs]) // the module is here but needs stuff
+        !isEmpty(modules[need].n) // the module is here but needs stuff
       ) {
         thisModule.c += 1
-        thisModule[needs][need] = true
+        thisModule.n[need] = true
       }
     })
 
@@ -98,10 +99,6 @@
 })(
   {},
   [],
-  0, // needs
-  1, // deps
-  2, // body
-  3, // factory
   window,
   'exports',
   'define',
